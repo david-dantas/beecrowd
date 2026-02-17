02bc370446ca0e1ca390469ad9aefa8adc2d951e
David <daviddantas21@gmail.com>
2026-02-17 13:23:49 -0300

    questoes-1011-1017

---
+++ 1012.py
@@ -0,0 +1,7 @@
+a, b, c = input().split()
+a = float(a)
+b = float(b)
+c = float(c)
+pi = 3.14159
+
+print(f"TRIANGULO: {(a*c)/2:.3f}\nCIRCULO: {pi*c**2:.3f}\nTRAPEZIO: {((a+b)*c)/2:.3f}\nQUADRADO: {b**2:.3f}\nRETANGULO: {a*b:.3f}")
---
+++ 1017.py
@@ -0,0 +1,4 @@
+a = int(input())
+b = int(input())
+gasto = (a*b)/12
+print(f"{gasto:.3f}")
